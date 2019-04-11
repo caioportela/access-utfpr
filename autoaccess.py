b5a140500a9a233e6c9dcbac5ed267dcd49cef82
Caio Portela <caio_fabiio@outlook.com>
2019-04-11 16:01:24 -0300

    Update autoaccess.py

--- autoaccess.py
+++ autoaccess.py
@@ -1,15 +1,15 @@
 import pyautogui as auto
 import time
 
-time.sleep(12)
+time.sleep(13)
 auto.press('winleft')
 time.sleep(0.5)
 auto.press(['f','i','r','e','f','o','x'], interval=1)
 auto.press('enter')
-time.sleep(12)
-auto.press(['i','n','t','r','a','n','e','t','.','p','b','.','u','t','f','p','r','.','e','d','u','.','b','r'], interval=1)
-auto.press('return')
-time.sleep(2)
+time.sleep(11)
+auto.press(['i','n','t','r','a','n','e','t','.','p','b','.','u','t','f','p','r','.','e','d','u','.','b','r'], interval=2)
 auto.press('return')
 time.sleep(3)
-auto.hotkey('alt', 'f4')
+auto.press('return')
+time.sleep(4)
+auto.hotkey('ctrl', 'w')
